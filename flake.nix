b6b77064d65b7d585dfb0566bfd871c8f8f8333f
Only1Thor <>
2024-06-28 23:18:41 +0200

    remove unstable repo (not used)

--- flake.nix
+++ flake.nix
@@ -1,13 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = {
     self,
     nixpkgs,
-    unstable,
     ...
   }: {
     nixosConfigurations = {
